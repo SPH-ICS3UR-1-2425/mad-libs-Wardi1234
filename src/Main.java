8a5807b3c298c91b6cb92256e499a03aba6c7484
Wardi1234 <Wardi.Hussein@stu.ocsb.ca>
2024-09-17 14:38:25 -0400

    Trial
    12

--- src/Main.java
+++ src/Main.java
@@ -13,17 +13,25 @@ public class Main {
 		 */
 		
 		Scanner in = new Scanner(System.in);
-		System.out.println("Welcome to Mad Libs!");
-		System.out.println("What's your name?");
+		System.out.println("You are currently under trial");
+		System.out.println("You are being charged as the greatest package theif of all time ");
+		System.out.println("State your Name");
 		String name = in.nextLine();
-		
+		System.out.println("Just How many packages did you steal"); 
         System.out.println("Give me a number");         
 	    int num1 = in.nextInt();
-        System.out.println("give me a place.");	
-	    //flush
+        System.out.println("Just how much money did you get from this");
+        System.out.println("Give me a number. Down to the cents");
+	    //flush    in.nextLine();
+        double decNumber = in.nextDouble(); 
         in.nextLine();
+        
         String place = in.nextLine();
         System.out.println("One day, "+name+ " wrote " +num1+ " Mad Libs projects in "+place  );
+    
+        
+        
+        
         
 	}
 
