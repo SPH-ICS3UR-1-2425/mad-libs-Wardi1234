b08b63e8a68d4c073d52345197de05471f753835
Wardi1234 <Wardi.Hussein@stu.ocsb.ca>
2024-09-19 14:39:17 -0400

    idk
    fixed up my code and stuff

--- src/Main.java
+++ src/Main.java
@@ -17,8 +17,8 @@ public class Main {
 		System.out.println("You are being charged as the greatest package theif of all time ");
 		System.out.println("State your Name");
 		String name = in.nextLine();
-		System.out.println("Just How many packages did you steal"); 
-        System.out.println("Give me a number");         
+		System.out.println("Just How many packages did you steal?"); 
+        System.out.println("Give me a number.");         
 	    int num1 = in.nextInt();
         System.out.println("Just how much money did you get from this");
         System.out.println("Give me a number. Down to the cents");
@@ -32,15 +32,36 @@ public class Main {
         System.out.println("Where did you keep the packages? give me a place");
         String place = in.nextLine();
         System.out.println("Why have you done all this?"); 
-        System.out.println("give us an anwser `")
-        
-        
-        System.out.println("One day, "+name+ " wrote " +num1+ " Mad Libs projects in "+name2  );
-    
-        
-        
-        
-        
+        System.out.println("Give us an anwser `");
+        String anwser = in.nextLine();
+        System.out.println("You had stolen a lot during your run");
+        System.out.println("What was your favorite thing you stole");
+        String Favthing = in.nextLine();
+        System.out.println("We noticed that you had an animal with you during your crimes");
+        System.out.println("What type of animal was it?");
+        String Animal = in.nextLine();
+        System.out.println("You've stolen packages from all over the place.");
+        System.out.println("Just how many cities have you been to during your run?");
+        int cities = in.nextInt();
+        in.nextLine();
+        System.out.println("You have had many nicknames throughout the years");
+        System.out.println("which one was your favorite?");
+        String Nickname = in.nextLine();
+        System.out.println(name+ ", the greatest Package theif of all time" );
+        System.out.println("You have been brought to trial today for your great crimes"); 
+        in.nextLine();
+        System.out.println("You have stolen" +num1+ "Packages, worth" +decNumber);
+        System.out.println(name+ "has stolen with" +name2+ "and a(n)"+Animal);
+        in.nextLine();
+        System.out.println("Their prized stolen possession was their" +Favthing);
+        System.out.println("They stored their stolen goods at"+place+"Stole from"+cities+"Cities");
+        in.nextLine(); 
+        System.out.println("The reason"+name+ "the"+Nickname+ "stole was because"+anwser);
+        in.nextLine();
+        System.out.println("Due to these reasons, Judge Higura sentences "+name+" To...");
+        System.out.println("Guilty! 900 consecutive life sentences is your punishment");
+        System.out.println("The end");
+	
 	}
 
 }
